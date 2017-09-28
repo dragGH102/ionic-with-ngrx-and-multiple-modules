2fd77277531f6107e74c00128ebd07d9810785f8
Manuel-Gajo <manuelgajo.mnl@gmail.com>
2017-09-28 12:03:31 +0200

    keyboard - auto open

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Keyboard } from '@ionic-native/keyboard';
 import { Store } from '@ngrx/store';
 import { AppState } from "../../shared/store/app.reducer";
 import { AddItem, DeleteItem, UpdateItem } from "../../modules/list/store/list/actions";
@@ -8,16 +9,22 @@ import makeItem from "../../modules/list/factories/item";
 @Component({
   selector: 'page-item-details',
   templateUrl: 'item-details.html',
+  providers: [
+    Keyboard,
+  ],
 })
 export class ItemDetailsPage {
   public selectedItem: any = {};
   public isNew = true;
   public action = 'Add';
 
+  @ViewChild('newItemInput') newItemInput;
+
   constructor(
     public _navParams: NavParams,
     private _navCtrl: NavController,
     private _store: Store<AppState>,
+    private _keyboard: Keyboard
   ) {
     // If we navigated to this page, we will have an item available as a nav param
     const selectedItem = _navParams.get('item');
@@ -49,5 +56,13 @@ export class ItemDetailsPage {
       item: this.selectedItem,
     }));
     this._navCtrl.pop();
+  }
+
+  ionViewDidLoad() {
+    // autofocus on input
+    setTimeout(()=>{
+      this.newItemInput.setFocus();
+      // this._keyboard.show();
+    }, 500)
   }
 }
